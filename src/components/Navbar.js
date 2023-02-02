a159ad34d87e83ab3ec721e34d8aa90a43a3c95e
sohaibasaad <46029942+sohaibasaad@users.noreply.github.com>
2023-02-02 17:50:24 +0500

    routes removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './css/Navbar.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
         <div>
             <nav className={`navbar navbar-expand-lg mainNav ${props.mode}`}>
                 <div className="container">
-                    <Link className="navbar-brand logo" to="/">{props.title}</Link>
+                    <a className="navbar-brand logo" href="/">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 mainNavUl">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
-                            <li className="nav-item">
+                            {/* <li className="nav-item">
                                 <Link className="nav-link" to="/about">{props.aboutText}</Link>
-                            </li>
+                            </li> */}
                         </ul>
                         {/* <form className="d-flex">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
