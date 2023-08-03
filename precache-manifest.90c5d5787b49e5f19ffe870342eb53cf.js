55e0036b677a0516795bb41d7de1adcd9e5190bc
sohaibasaad <46029942+sohaibasaad@users.noreply.github.com>
2023-08-03 11:49:45 +0500

    Updates

---
+++ precache-manifest.90c5d5787b49e5f19ffe870342eb53cf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3f9f3e36c77cbad6b10",
+    "url": "/demo-react/static/css/main.21477dcf.chunk.css"
+  },
+  {
+    "revision": "e3f9f3e36c77cbad6b10",
+    "url": "/demo-react/static/js/main.e3f9f3e3.chunk.js"
+  },
+  {
+    "revision": "c76d7d2072ec88a191f3",
+    "url": "/demo-react/static/css/1.932a38c8.chunk.css"
+  },
+  {
+    "revision": "c76d7d2072ec88a191f3",
+    "url": "/demo-react/static/js/1.c76d7d20.chunk.js"
+  },
+  {
+    "revision": "540a7911fb6c502fad2c",
+    "url": "/demo-react/static/js/runtime~main.540a7911.js"
+  },
+  {
+    "revision": "0c270243366fac77fa91e01e42853b92",
+    "url": "/demo-react/static/media/icon-2.0c270243.png"
+  },
+  {
+    "revision": "08ec843fab25c2c7cae8c8f281ac2a8e",
+    "url": "/demo-react/static/media/icon-3.08ec843f.png"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/demo-react/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "c0929a1e055f2ceb5c889501c24a24e1",
+    "url": "/demo-react/static/media/primeicons.c0929a1e.woff2"
+  },
+  {
+    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
+    "url": "/demo-react/static/media/primeicons.c08d00ca.eot"
+  },
+  {
+    "revision": "062070972fbad0f4fbb3505e6d013a39",
+    "url": "/demo-react/static/media/primeicons.06207097.woff"
+  },
+  {
+    "revision": "acb19296cb26b15dcf42e13c603217f2",
+    "url": "/demo-react/static/media/primeicons.acb19296.ttf"
+  },
+  {
+    "revision": "1d854e3ab2c37173804a0d1d1af229ee",
+    "url": "/demo-react/static/media/primeicons.1d854e3a.svg"
+  },
+  {
+    "revision": "c350b061e10c0eba1ce38f042b6e3b67",
+    "url": "/demo-react/index.html"
+  }
+];
